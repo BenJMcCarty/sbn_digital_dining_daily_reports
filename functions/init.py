2796fb2ecbd5da59c55311eb669a17bdff02239a
Ben McCarty <bmccarty505@gmail.com>
2022-04-19 15:18:08 -0400

    Fixed workflow loop

--- functions/init.py
+++ functions/init.py
@@ -1,6 +1,6 @@
 
 
-from functions import loop_script
+# from functions import loop_script
 from functions import script
 from functions import cleaning_2
 from functions import cleaning_function
--- functions/loop_script.py
+++ functions/loop_script.py
@@ -1,19 +1,19 @@
-## Looping through workflow
+# ## Looping through workflow
 
-import pandas as pd
-from functions.script import workflow
+# import pandas as pd
+# from functions.script import workflow
 
-def loop_workflow(summary_file_name, receipts_filepath, log_filepath):
+# def loop_workflow(summary_file_name, receipts_filepath, log_filepath):
 
     
-    list_mops = ['ag', 'rc', 'ax', 'ca', 'di', 'vimc']
-    new_results = []
-    for mop in list_mops:
-        file_path = f'./data/02_25_2022_mop_det_{mop}.xls'
-        summary_file_name = f'results_for_{mop}.xlsx'
-        new_results.append(workflow(data_filepath = file_path, summary_file_name = summary_file_name,
-                                    receipts_filepath = receipts_filepath, log_filepath = log_filepath))
+#     list_mops = ['ag', 'rc', 'ax', 'ca', 'di', 'vimc']
+#     new_results = []
+#     for mop in list_mops:
+#         file_path = f'./data/02_25_2022_mop_det_{mop}.xls'
+#         summary_file_name = f'results_for_{mop}.xlsx'
+#         new_results.append(workflow(data_filepath = file_path, summary_file_name = summary_file_name,
+#                                     receipts_filepath = receipts_filepath, log_filepath = log_filepath))
         
-    new_results_df = pd.concat(new_results)
+#     new_results_df = pd.concat(new_results)
 
-    return new_results_df
+#     return new_results_df
